60af6e0ad31ccaa1fd9224f4de79b5aaffbe7086
JGODEFROY18 <90181985+JGODEFROY18@users.noreply.github.com>
2022-09-05 13:05:10 +0200

    first commit

---
+++ User.php
@@ -0,0 +1,40 @@
+<?php
+    class User{
+
+        //Propriétés private
+        private $id_;
+        private $isAdmin_ = False;
+        private $login_;
+
+
+
+
+        //Méthode public
+        public function __construct($id,$isAdmin,$login){
+            $this->id_ = $id;
+            $this->isAdmin_ = $isAdmin;
+            $this->login_ = $login;
+
+        }
+
+
+        public function seConnecter($login,$pass){
+            $RequetSql = "SELECT * FROM 'User
+            WHERE 
+            'login' = '".$_POST['login]."'
+            AND
+            'pass' = '".$_POST['passs']."';
+
+            $resultat = $pdo->query($RequetSql); //resultat sera de type pdoStatement
+            if ($resultat->rowCount()>0){
+                //echo "on a trouver le bon login";
+                $_SESSION['Connexion']= true;
+                return true;
+            }else{
+                //echo "le login ".$_POST['Login']." et le pass".$_POST['pass']." n'est pas bon";
+                return false;
+            }
+        }
+
+    }
+?>
